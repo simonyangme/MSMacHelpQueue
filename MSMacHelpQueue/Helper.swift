0d4b5462246ca18c435bce1c1f1604073c94d3c1
Simon Yang <lokisnake@gmail.com>
2015-07-01 15:48:18 -0700

    Switching to WebView

--- MSMacHelpQueue/Helper.swift
+++ MSMacHelpQueue/Helper.swift
@@ -10,22 +10,22 @@ import Foundation
 import ReactiveCocoa
 import WebKit
 
-typealias RequestBundle = (WKWebView, Int, Request)
+typealias RequestBundle = (WebView, Int, Request)
 typealias RequestBundleSignal = SignalProducer<RequestBundle, NoError>
 
 func extractNames(input: RequestBundleSignal) -> RequestBundleSignal {
     return input |> flatMap(.Merge) { webView, index, request in
         return RequestBundleSignal() { sink, disposable in
-            webView.evaluateJavaScript("document.getElementsByClassName('event-author-wrap mb2')[\(index)].getElementsByTagName('span')[0].innerHTML") { result, error in
-                println(result)
-                if let name = result as? String {
-                    var newRequest = request
-                    newRequest.name = name
-                    sendNext(sink, (webView, index, newRequest))
-                    sendCompleted(sink)
-                } else {
-                    sendCompleted(sink)
-                }
+            let result = webView.stringByEvaluatingJavaScriptFromString("document.getElementsByClassName('event-author-wrap mb2')[\(index)].getElementsByTagName('span')[0].innerHTML")
+            println(result)
+            if let name = result {
+                var newRequest = request
+                newRequest.name = name
+                sendNext(sink, (webView, index, newRequest))
+                sendCompleted(sink)
+            } else {
+                // TODO: Should error out here
+                sendCompleted(sink)
             }
         }
     }
@@ -34,16 +34,14 @@ func extractNames(input: RequestBundleSignal) -> RequestBundleSignal {
 func extractTitles(input: RequestBundleSignal) -> RequestBundleSignal {
     return input |> flatMap(.Merge) { webView, index, request in
         return RequestBundleSignal() { sink, disposable in
-            webView.evaluateJavaScript("document.getElementsByClassName('event-time mb1 mt0')[\(index)].innerHTML") { result, error in
-//                println(result)
-                if let questionTitle = result as? String {
-                    var newRequest = request
-                    newRequest.title = questionTitle
-                    sendNext(sink, (webView, index, newRequest))
-                    sendCompleted(sink)
-                } else {
-                    sendCompleted(sink)
-                }
+            let result = webView.stringByEvaluatingJavaScriptFromString("document.getElementsByClassName('event-time mb1 mt0')[\(index)].innerHTML")
+            if let questionTitle = result {
+                var newRequest = request
+                newRequest.title = questionTitle
+                sendNext(sink, (webView, index, newRequest))
+                sendCompleted(sink)
+            } else {
+                sendCompleted(sink)
             }
         }
     }
@@ -52,16 +50,14 @@ func extractTitles(input: RequestBundleSignal) -> RequestBundleSignal {
 func extractQuestions(input: RequestBundleSignal) -> RequestBundleSignal {
     return input |> flatMap(.Merge) { webView, index, request in
         return RequestBundleSignal() { sink, disposable in
-            webView.evaluateJavaScript("document.getElementsByClassName('event-content pt2 mb2')[\(index)].getElementsByTagName('span')[0].getElementsByTagName('p')[0].innerHTML") { result, error in
-                println(result)
-                if let question = result as? String {
-                    var newRequest = request
-                    newRequest.question = question
-                    sendNext(sink, (webView, index, newRequest))
-                    sendCompleted(sink)
-                } else {
-                    sendCompleted(sink)
-                }
+            let result = webView.stringByEvaluatingJavaScriptFromString("document.getElementsByClassName('event-content pt2 mb2')[\(index)].getElementsByTagName('span')[0].getElementsByTagName('p')[0].innerHTML")
+            if let question = result {
+                var newRequest = request
+                newRequest.question = question
+                sendNext(sink, (webView, index, newRequest))
+                sendCompleted(sink)
+            } else {
+                sendCompleted(sink)
             }
         }
     }
--- MSMacHelpQueue/ViewController.swift
+++ MSMacHelpQueue/ViewController.swift
@@ -12,64 +12,65 @@ import ReactiveCocoa
 
 class ViewController: NSViewController {
     /// some web view
-    var webView: WKWebView!
-    var finishedNavigationSignal: Signal<WKWebView, NoError>!
-    var finishedNavigationSink: SinkOf<Event<WKWebView, NoError>>!
+    @IBOutlet var webView: WebView!
+    var finishedNavigationSignal: Signal<WebView, NoError>!
+    var finishedNavigationSink: SinkOf<Event<WebView, NoError>>!
     var requestsSignalProducer: SignalProducer<Request, NoError>!
     
-    override func loadView() {
-//        super.loadView()
-//        self.invalidateRestorableState()
-
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-        var url = NSURL(string:"https://www.makeschool.com/sa/admin")
-        var req = NSURLRequest(URL:url!)
-        self.webView!.loadRequest(req)
+        self.webView.frameLoadDelegate = self
+        self.webView.resourceLoadDelegate = self
+        self.webView.mainFrameURL = "https://www.makeschool.com/sa/admin"
         
         // Send WKWebViews when web view finishes loading
-        let (pipeSignal, pipeSink) = Signal<WKWebView, NoError>.pipe()
-        finishedNavigationSignal = pipeSignal |> delay(4, onScheduler: QueueScheduler.mainQueueScheduler)
+        let (pipeSignal, pipeSink) = Signal<WebView, NoError>.pipe()
+        finishedNavigationSignal = pipeSignal |> delay(4, onScheduler: QueueScheduler.mainQueueScheduler) |> throttle(2, onScheduler: QueueScheduler.mainQueueScheduler)
         finishedNavigationSink = pipeSink
         
         // Tries to hide unnecessary parts of the DOM
         timer(1, onScheduler: QueueScheduler.mainQueueScheduler).start(next: {_ in
-            self.webView.evaluateJavaScript("document.getElementsByClassName('col-three-fifths mt0 mb0')[0].style.display = 'none'", completionHandler: nil)
+            self.webView.stringByEvaluatingJavaScriptFromString("document.getElementsByClassName('col-three-fifths mt0 mb0')[0].style.display = 'none'")
         })
         
-        timer(30, onScheduler: QueueScheduler.mainQueueScheduler).start(next: {_ in self.webView.reload() })
+        // Reload page
+        timer(60, onScheduler: QueueScheduler.mainQueueScheduler).start(next: {_ in
+            self.webView.reload(nil)
+        })
         
         requestsSignalProducer = SignalProducer {sink, disposable in
             var requests = [Request]()
             self.finishedNavigationSignal.observe(next: { webView in
-                webView.evaluateJavaScript("document.getElementsByClassName('event-author-wrap mb2').length") { result, error in
-                    if let resultCount = result as? Int {
-                        var requestSignalProducers = [SignalProducer<RequestBundle, NoError>]()
-                        for i in 0..<resultCount {
-                            requestSignalProducers.append(SignalProducer { sink, disposable in
-                                sendNext(sink, (webView, i, Request()))
-                                sendCompleted(sink)
-                            } |> extractNames |> extractTitles |> extractQuestions)
-                        }
-                        
-                        let requestsSignal = SignalProducer<SignalProducer<RequestBundle, NoError>, NoError>(values: requestSignalProducers)
-                            |> flatten(.Concat)
-                            |> map { _, _, request in return request }
-                            |> on(started: {println("started")}, event: {e in println(e)} )
-                        requestsSignal.start(sink)
+                let resultCount = webView.stringByEvaluatingJavaScriptFromString("document.getElementsByClassName('event-author-wrap mb2').length")?.toInt()
+                if let resultCount = resultCount {
+                    var requestSignalProducers = [SignalProducer<RequestBundle, NoError>]()
+                    for i in 0..<resultCount {
+                        requestSignalProducers.append(SignalProducer { sink, disposable in
+                            sendNext(sink, (webView, i, Request()))
+                            sendCompleted(sink)
+                        } |> extractNames |> extractTitles |> extractQuestions)
                     }
+                    
+                    let requestsSignal = SignalProducer<SignalProducer<RequestBundle, NoError>, NoError>(values: requestSignalProducers)
+                        |> flatten(.Concat)
+                        |> map { _, _, request in return request }
+//                        |> on(started: {println("started")}, event: {e in println(e)} )
+                        |> filter { request in
+                            if !contains(requests, request) {
+                                requests.append(request)
+                                return true
+                            } else {
+                                return false
+                            }
+                        }
+                    requestsSignal.start(sink)
                 }
             })
         }
         
         requestsSignalProducer.start(next: { request in
-            println(request)
+//            println(request)
             let notification = NSUserNotification()
             notification.title = request.name
             notification.subtitle = request.title
@@ -81,25 +82,25 @@ class ViewController: NSViewController {
     }
 
     @IBAction func refresh(sender: AnyObject) {
-        webView.reload()
+        webView.reload(nil)
     }
 
 }
 
-extension ViewController: WKNavigationDelegate {
-    
-    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
-        if let url = webView.URL, lastPathComponent = url.lastPathComponent {
+extension ViewController {
+    override func webView(sender: WebView!, didFinishLoadForFrame frame: WebFrame!) {
+        if let url = frame.dataSource?.request.URL, lastPathComponent = url.lastPathComponent {
             switch lastPathComponent {
             case "login":
                 println("login!")
-                webView.evaluateJavaScript("document.forms[0].elements[2].value = 'fill.it.in@makeschool.com'", completionHandler:nil)
-                webView.evaluateJavaScript("document.forms[0].elements[3].value=''", completionHandler:nil)
-                webView.evaluateJavaScript("document.forms[0].elements[5].click()", completionHandler:nil)
+                webView.stringByEvaluatingJavaScriptFromString("document.forms[0].elements[2].value = 'fill.it.in@makeschool.com'")
+                webView.stringByEvaluatingJavaScriptFromString("document.forms[0].elements[3].value=''")
+                webView.stringByEvaluatingJavaScriptFromString("document.forms[0].elements[5].click()")
             case "admin":
-                sendNext(finishedNavigationSink, webView)
+                sendNext(finishedNavigationSink, sender)
             default:
                 println("unrecognized! \(lastPathComponent)")
+                break
             }
         }
     }
